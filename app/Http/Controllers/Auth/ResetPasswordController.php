731f78bcfa73ae41e36fb2d7877b31b3d9d9409e
ruanwenwu <379879523@qq.com>
2019-10-15 10:01:27 +0000

    reset password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -35,5 +36,10 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function sendResetResponse(Request $request,$response){
+        session()->flash('success','重置密码成功，您已登录');
+        return redirect($this->redirectPath());
     }
 }
