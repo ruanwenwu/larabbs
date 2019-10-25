9710cdcede04efbe14b146883243a3c3424311ff
ruanwenwu <379879523@qq.com>
2019-10-25 22:40:16 +0000

    add order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -15,4 +15,28 @@ class Topic extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    //话题排序本地作用域
+    public function scopeWithOrder($query,$order){
+        switch($order){
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+    }
+
+    //最近排序
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at','DESC');
+    }
+
+    //最新回复排序
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at', 'DESC');
+    }
 }
